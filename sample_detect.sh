f79bf3d051f015511647d31144740e5f9a310a6c
Jonathan MFR Chris <a27182818284590e@gmail.com>
2023-10-14 04:31:24 -0600

    u

--- sample_detect.sh
+++ sample_detect.sh
@@ -7,7 +7,7 @@ do
   newest_folder=$folder; 
 done
 newest_weight="$newest_folder/weights/best.pt"
-echo "newest trained weight is at: $newest_weight"
+echo "newest trained weight is at: ImageRecog/yolov5/$newest_weight"
 
 # train with the newest train weights.
 python detect.py --weights $newest_weight --img 640 --conf 0.25 --source ../DetectImages

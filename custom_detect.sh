0491b36756ec89c6fb9ddb7295fc9c75994254c9
Jonathan MFR Chris <a27182818284590e@gmail.com>
2023-10-14 03:28:46 -0600

    u

---
+++ custom_detect.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# get the newest train weights.
+newest_folder=""
+for folder in runs/train/* 
+do 
+  newest_folder=$folder; 
+done
+newest_weight="$newest_folder/weights/best.pt"
+echo "newest trained weight is at: $newest_weight"
+
+# train with the newest train weights.
+python detect.py --weights $newest_weight --img 640 --conf 0.25 --source ../../DetectImages
+
+# transfer the detected images to outer folder
+rsync -a --update runs/detect ../../
